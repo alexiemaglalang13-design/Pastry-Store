33951302207839eefc110374388e0efeb3b8af16
alexiemaglalang13-design <alexiemaglalang13@gmail.com>
2025-11-29 20:32:31 +0800

    Added card and updated content for conditional.php

--- Activity 2/conditional.php
+++ Activity 2/conditional.php
@@ -1,28 +1,31 @@
 <?php require 'header.php'; ?>
+<h1> Daily Updates </h1>
 
-<h1> Conditional Statements in Our Pastry Shop</h1>
-
-<!-- Discount Section -->
+<!-- If-Else -->
 <div class="conditional-card">
     <?php
-    $hour = 14;
+    $month = "december";
 
-    if ($hour >= 12) {
-        echo "<p> You are eligible for our <strong>12% Christmas Discount!</strong></p>";
+    if ($month >= "december") {
+        echo "<p>Sweet Cravings is getting ready to bake your <strong>Best Christmas Pastries</strong> this coming holidays!</p>";
     } else {
-        echo "<p>Discount starts at <strong>12 noon</strong>. Please come back later!</p>";
+        echo "<p>Sorry. The 12% discount promo is only available on December 12 to 23.";
     }
     ?>
 </div>
 
-<!-- Store Hours Section -->
+<div class="conditional-card">
+    <p><strong>Anniversary Promo!</strong> To celebrate our 12th year anniversary, Sweet Cravings shop will offer 12% discount on all pastries. </p>
+</div>
+
+<!-- If-ElseIf -->
 <div class="conditional-card">
     <?php
-    $time = 18;
+    $time = 12;
 
     if ($time < 12) {
         echo "<p>The store is preparing pastries. Opening soon!</p>";
-    } elseif ($time <= 20) {
+    } elseif ($time <= 12) {
         echo "<p>We are <strong>OPEN</strong>! Come grab your treats </p>";
     } else {
         echo "<p>We are now closed. Please visit us tomorrow.</p>";
@@ -30,7 +33,7 @@
     ?>
 </div>
 
-<!-- Pastry Availability Section -->
+<!-- Switch Case -->
 <div class="conditional-card">
     <?php
     $pastry = "Cinnamon Roll";
