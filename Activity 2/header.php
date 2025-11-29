56f6cd32f23b9dd5f4baa0029100e68771ca1e7e
alexiemaglalang13-design <alexiemaglalang13@gmail.com>
2025-11-29 19:12:05 +0800

    Create header.php

---
+++ Activity 2/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Sweet Cravings Pastry Shop</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+<nav>
+    <a href="pastry.php">Home</a>
+    <a href="conditional.php">Conditional Statements</a>
+    <a href="loop.php">Looping</a>
+</nav>
+
+<img src="side.png" class="right-image">
